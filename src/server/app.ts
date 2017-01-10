cda8d3011bacf9773240396d319d6e75786c8fd5
creativefeather <creativefeather@outlook.com>
2017-01-10 06:38:15 -0700

    good start small mod

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,7 +1,7 @@
 var express = require('express'),
     app = express();
 
-app.all(function(req, res, next) {
+app.all('/', function(req, res, next) {
   res.end('You successfully requested page.');
 });
 
