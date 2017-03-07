56152e7105ed98207bee9ba4edc37357adf825e5
Jesse Bird <j.hey.bird@gmail.com>
2017-03-07 14:57:49 -0700

    current: nodeschool goingnative myaddon

---
+++ src/native-addons/myaddon/index.js
@@ -0,0 +1,3 @@
+const addon = require('bindings')('myaddon');
+
+console.log(addon.length(process.argv[2]));
---
+++ src/native-addons/myaddon/myaddon.cc
@@ -0,0 +1,62 @@
+//hello.cc
+#include <node.h>
+#include <nan.h>
+
+using namespace v8;
+
+
+// #include <node.h>
+
+// namespace demo {
+
+// using v8::FunctionCallbackInfo;
+// using v8::Isolate;
+// using v8::Local;
+// using v8::Object;
+// using v8::String;
+// using v8::Value;
+
+NAN_METHOD(Print) {
+  Nan::MaybeLocal<String> maybeString = Nan::To<String>(info[0]);
+  if (maybeString.IsEmpty() == false) {
+    Local<String> str = maybeString.ToLocalChecked();
+    printf("%s\n", *String::Utf8Value(str));
+  }
+}
+
+NAN_METHOD(Length) {
+  Nan::HandleScope scope;
+
+  Nan::MaybeLocal<String> mStr = Nan::To<String>(info[0]);
+  if (!mStr.IsEmpty()) {
+    Local<String> str = mStr.ToLocalChecked();
+    int len = strlen(*String::Utf8Value(str));
+    //printf("length: %d\n", len);
+
+    Local<Number> length = Nan::New<Number>(len);
+    info.GetReturnValue().Set(length);
+  }
+}
+
+// void Method(const FunctionCallbackInfo<Value>& args) {
+//   Isolate* isolate = args.GetIsolate();
+//   args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
+// }
+
+NAN_MODULE_INIT(Init) {
+  Nan::Set(target, 
+           Nan::New("print").ToLocalChecked(),
+           Nan::GetFunction(Nan::New<FunctionTemplate>(Print)).ToLocalChecked());
+  
+  Nan::Set(target,
+           Nan::New("length").ToLocalChecked(),
+           Nan::GetFunction(Nan::New<FunctionTemplate>(Length)).ToLocalChecked());
+}
+
+// void Init(Local<Object> exports) {
+//   NODE_SET_METHOD(exports, "hello", Method);
+//}
+
+NODE_MODULE(myaddon, Init)
+
+//}  // namespace demo
